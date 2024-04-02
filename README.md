7fc732ce7fd1ec0a8382c4ab2d2e48c9c2e2f163
Krant Lee <krantlee@Krants-MacBook-Pro.local>
2024-04-02 15:41:48 -0700

    add dataset

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # TECHIN-510-Lab2
+
+## Overview
+
+This is a Boston Housing Assistant App by Krant Lee
+
+## How to run
+
+Open the terminal and run the following commands:
+```
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+```
+steamlit run ap.py
+```
+
+## What's Included
+
+- `app.py`: The main application
+
+## Lessons learned
+- 
+
+## Questions
+- 
--- app.py
+++ app.py
@@ -3,15 +3,23 @@ import pandas as pd
 import seaborn as sns
 
 st.set_page_config(
-    page_title="Penguins Explorer",
-    page_icon="🐧",
+    page_title="Boston Housing Assistant",
+    page_icon="🌆",
     layout="centered", # centered, wide 
     initial_sidebar_state="auto", 
     menu_items=None
 )
 
-st.title("🐧 Penguins Explorer")
+st.title("🌆 Boston Housing Assistant")
 
-df = pd.read_csv('https://raw.githubusercontent.com/mcnakhaee/palmerpenguins/master/palmerpenguins/data/penguins.csv')
+st.write("## Raw Data")
+
+df = pd.read_csv('/Users/krantlee/Documents/Study/University of Washington/TECHIN 510/Lab2/TECHIN-510-Lab2/Boston housing dataset.zip')
 
 st.write(df)
+
+# 对所有列使用均值填充缺失值
+df_filled = df.fillna(df.mean())
+
+# 验证缺失值处理后的结果
+print(df_filled.isnull().sum())
